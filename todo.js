5cafd41187a3444826b8d52cdc5d7507a0f3ae70
Schmokes <lesa22kz@student.ju.se>
2023-04-10 10:35:32 +0200

    working in a way

--- todo.js
+++ todo.js
@@ -1,26 +1,61 @@
 const taskListElement = document.getElementById("taskList");
+const addTaskElement = document.getElementById("addTask");
+const inputElement = document.getElementById("inputField");
 
 //string the data in an array?
-let tasks = [];
+let taskList = [];
+let taskName = inputElement.value;
+addTaskElement.addEventListener("click", addTaskToList);
+
+function addTaskToList() {
+  /* taskListElement.innerHTML = ""; */
+
+  if (inputElement.value.length > 0) {
+    /*  taskList.push(taskName);
+    localStorage.setItem("Tasks", JSON.stringify(taskList)); */
+    createTask();
+    console.log("click works");
+  }
+}
 
 function createTask() {
-  /* <div class="taskStyle" id="taskList">
+  /* <div class="designTask">
           <p>⚪</p>
           <p>❌</p>
           <span>Task</span>
         </div> */
-
+  console.log("in create task");
+  const divElement = document.createElement("div");
+  divElement.classList.add("designTask");
+  taskListElement.appendChild(divElement);
+  console.log("div element");
+  let taskDone = false;
   const checkmarkElement = document.createElement("p");
   checkmarkElement.innerText = "⚪";
-  taskListElement.appendChild(checkmarkElement);
 
+  //marking the task as done
+  checkmarkElement.addEventListener("click", () => {
+    if (taskDone === false) {
+      checkmarkElement.innerText = "🌸";
+      taskDone = true;
+    } else if (taskDone === true) {
+      checkmarkElement.innerText = "⚪";
+      taskDone = false;
+    }
+  });
+  divElement.appendChild(checkmarkElement);
+  console.log("checkmark element");
   const removeElement = document.createElement("p");
   removeElement.innerText = "❌";
-  taskListElement.appendChild(removeElement);
+  divElement.appendChild(removeElement);
+  console.log("remove element");
+  const storedTasksJSON = localStorage.getItem("Tasks");
+  const storedTasks = JSON.parse(storedTasksJSON);
 
   const taskNameElement = document.createElement("span");
-  taskListElement.appendChild(taskListElement);
+  taskNameElement.innerText = inputElement.value;
+  console.log(inputElement.value);
+  divElement.appendChild(taskNameElement);
 
-  //function for finishing a task
   //function for removing the task
 }
